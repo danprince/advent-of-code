f3db25a6a4dbef450eea2f022a966e0b05af3b85
danprince <danielprince@hotmail.co.uk>
2017-12-04 08:22:31 +0000

    add part two solutions for day two

---
+++ day-2/part-two.clj
@@ -0,0 +1,20 @@
+(def sort-by-largest
+  (comp reverse sort))
+
+(defn find-even-divisions [[x & ys]]
+  (concat
+    (for [y ys :let [z (/ x y)] :when (integer? z)] z)
+    (when (not-empty ys) (find-even-divisions ys))))
+
+(defn checksum [sheet]
+  (->> sheet
+    (map sort-by-largest)
+    (map find-even-divisions)
+    (flatten)
+    (reduce +)))
+
+(def sheet [[5 9 2 8]
+            [9 4 7 3]
+            [3 8 6 5]])
+
+(assert (= (checksum sheet) 9))
