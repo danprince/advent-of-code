cb4af67ad640420a4f3568ce0f812e4b83024ee2
danprince <danielprince@hotmail.co.uk>
2017-12-22 10:02:08 +0000

    make scala solutions exportable

--- day-10-scala/PartOne.scala
+++ day-10-scala/PartOne.scala
@@ -1,41 +1,43 @@
 // http://adventofcode.com/2017/day/10
 
-package advent.part.one
+package advent.day10
 
 case class State( items : Array[Int] , position : Int, skip: Int )
 
-def circular(index: Int, items: Array[Int]): Int = {
-  index % items.length
-}
+object PartOne {
+  def circular(index: Int, items: Array[Int]): Int = {
+    index % items.length
+  }
 
-def reverseThrough(items: Array[Int], pos: Int, len: Int): Array[Int] = {
-  val through = for {
-    index <- pos until pos + len
-    real = circular(index, items)
-  } yield real
+  def reverseThrough(items: Array[Int], pos: Int, len: Int): Array[Int] = {
+    val through = for {
+      index <- pos until pos + len
+      real = circular(index, items)
+    } yield real
 
-  val lookups =
-    (items.indices zip items.indices).toMap ++
-    (through zip through.reverse).toMap
+    val lookups =
+      (items.indices zip items.indices).toMap ++
+      (through zip through.reverse).toMap
 
-  val values = for {
-    x <- items.indices
-    y <- lookups get x
-  } yield items(y)
+    val values = for {
+      x <- items.indices
+      y <- lookups get x
+    } yield items(y)
 
-  values.toArray
-}
+    values.toArray
+  }
 
-def knot(state: State, length: Int): State = {
-  val items = reverseThrough(state.items, state.position, length)
-  val position = state.position + state.skip + length
-  val skip = state.skip + 1
-  State(items, position, skip)
-}
+  def knot(state: State, length: Int): State = {
+    val items = reverseThrough(state.items, state.position, length)
+    val position = state.position + state.skip + length
+    val skip = state.skip + 1
+    State(items, position, skip)
+  }
 
-def solve(lengths: List[Int]): Int = {
-  val items = Array.range(0, 256)
-  val init = State(items, 0, 0)
-  val result = lengths.foldLeft(init)(knot)
-  result.items(0) * result.items(1)
+  def solve(lengths: List[Int]): Int = {
+    val items = Array.range(0, 256)
+    val init = State(items, 0, 0)
+    val result = lengths.foldLeft(init)(knot)
+    result.items(0) * result.items(1)
+  }
 }
--- day-10-scala/PartTwo.scala
+++ day-10-scala/PartTwo.scala
@@ -1,39 +1,38 @@
 // http://adventofcode.com/2017/day/10
 
-package advent.part.two
+package advent.day10
 
-import advent.part.one.{State, knot}
+object PartTwo {
+  def xor(x: Int, y: Int): Int = x ^ y
 
-def xor(x: Int, y: Int): Int = x ^ y
+  def toHex(x: Int): String = {
+    val s = x.toHexString
 
-def toHex(x: Int): String = {
-  val s = x.toHexString
-
-  s.length match {
-    case 1 => "0" ++ s
-    case _ => s
-  }
-}
-
-def compact(items: Array[Int]): String = {
-  val nums = for {
-    block <- items.sliding(16, 16)
-    number = block.reduce(xor)
-    hex = toHex(number)
-  } yield hex
-
-  nums.mkString("")
-}
-
-def solve(input: String): String = {
-  val lengths = input.toList.map { x => x.toInt } ++ List(17, 31, 73, 47, 23)
-  val items = Array.range(0, 256)
-  var state = State(items, 0, 0)
-
-  for (x <- 0 until 64) {
-    state = lengths.foldLeft(state)(knot)
+    s.length match {
+      case 1 => "0" ++ s
+      case _ => s
+    }
   }
 
-  compact(state.items)
-}
+  def compact(items: Array[Int]): String = {
+    val nums = for {
+      block <- items.sliding(16, 16)
+      number = block.reduce(xor)
+      hex = toHex(number)
+    } yield hex
 
+    nums.mkString("")
+  }
+
+  def solve(input: String): String = {
+    val lengths = input.toList.map { x => x.toInt } ++ List(17, 31, 73, 47, 23)
+    val items = Array.range(0, 256)
+    var state = State(items, 0, 0)
+
+    for (x <- 0 until 64) {
+      state = lengths.foldLeft(state)(PartOne.knot)
+    }
+
+    compact(state.items)
+  }
+}
