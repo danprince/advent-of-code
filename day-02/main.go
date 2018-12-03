c0f31fb4fa64f1d96611235a4c73baca2d0d4b50
danprince <danielprince@hotmail.co.uk>
2018-12-03 12:51:50 +0000

    adds day 2 solutions

---
+++ day-02/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func SolvePartOne(input string) int {
+	ids := strings.Split(input, "\n")
+
+	twos := 0
+	threes := 0
+
+	for _, id := range ids {
+		buckets := map[rune]int{}
+
+		for _, r := range id {
+			buckets[r] += 1
+		}
+
+		for _, n := range buckets {
+			if n == 2 {
+				twos++
+				break
+			}
+		}
+
+		for _, n := range buckets {
+			if n == 3 {
+				threes++
+				break
+			}
+		}
+	}
+
+	return twos * threes
+}
+
+func SolvePartTwo(input string) string {
+	ids := strings.Split(input, "\n")
+
+	for _, a := range ids {
+		for _, b := range ids {
+			if a == b {
+				continue
+			}
+
+			diff := 0
+			mark := 0
+
+			for i := range a {
+				if a[i] != b[i] {
+					diff += 1
+					mark = i
+				}
+			}
+
+			if diff == 1 {
+				return a[:mark] + b[mark+1:]
+			}
+		}
+	}
+
+	return ""
+}
+
+func main() {
+	bytes, _ := ioutil.ReadAll(os.Stdin)
+	input := strings.TrimSpace(string(bytes))
+	fmt.Println("Part 1:", SolvePartOne(input))
+	fmt.Println("Part 2:", SolvePartTwo(input))
+}
---
+++ day-02/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+const example1 = `abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab`
+
+const example2 = `abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz`
+
+func TestSolvePartOne(t *testing.T) {
+	expected := 12
+	actual := SolvePartOne(example1)
+
+	if actual != expected {
+		t.Errorf(`Part 1: Expected: %d Actual: %d`, expected, actual)
+	}
+}
+
+func TestSolvePartTwo(t *testing.T) {
+	expected := "fgij"
+	actual := SolvePartTwo(example2)
+
+	if actual != expected {
+		t.Errorf(`Part 2: Expected: "%s" Actual: "%s"`, expected, actual)
+	}
+}
