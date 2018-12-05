705d893b499b9f502b7cf484858e44c86d1ea7a0
danprince <danielprince@hotmail.co.uk>
2018-12-05 11:26:36 +0000

    adds day 5 solutions

---
+++ day-05/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+	"unicode"
+)
+
+func React(polymer []rune) []rune {
+	stack := []rune{}
+
+	for _, b := range polymer {
+		if len(stack) == 0 {
+			stack = append(stack, b)
+			continue
+		}
+
+		a := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if a == b || unicode.ToUpper(a) != unicode.ToUpper(b) {
+			stack = append(stack, a, b)
+		}
+	}
+
+	return stack
+}
+
+func SolvePartOne(input string) int {
+	polymer := React([]rune(input))
+	return len(polymer)
+}
+
+func SolvePartTwo(input string) int {
+	polymer := []rune(input)
+	runes := map[rune]bool{}
+
+	for _, r := range polymer {
+		runes[unicode.ToUpper(r)] = true
+	}
+
+	score := len(input)
+
+	for r := range runes {
+		regex := regexp.MustCompile("(?i)" + string(r))
+		str := regex.ReplaceAllString(input, "")
+		polymer := React([]rune(str))
+
+		if len(polymer) < score {
+			score = len(polymer)
+		}
+	}
+
+	return score
+}
+
+func main() {
+	bytes, _ := ioutil.ReadAll(os.Stdin)
+	input := strings.TrimSpace(string(bytes))
+	fmt.Println("Part 1:", SolvePartOne(input))
+	fmt.Println("Part 2:", SolvePartTwo(input))
+}
---
+++ day-05/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestSolvePartOne(t *testing.T) {
+	examples := map[string]int{
+		"aA":               len(""),
+		"abBA":             len(""),
+		"abAB":             len("abAB"),
+		"aabAAB":           len("aabAAB"),
+		"dabAcCaCBAcCcaDA": len("dabCBAcaDA"),
+	}
+
+	for input, output := range examples {
+		expected := output
+		actual := SolvePartOne(input)
+
+		if actual != expected {
+			t.Errorf(`Part 1: Expected: %#v Actual: %#v`, expected, actual)
+		}
+	}
+}
+
+func TestSolvePartTwo(t *testing.T) {
+	example := `dabAcCaCBAcCcaDA`
+	expected := 4
+	actual := SolvePartTwo(example)
+
+	if actual != expected {
+		t.Errorf(`Part 2: Expected: %#v Actual: %#v`, expected, actual)
+	}
+}
