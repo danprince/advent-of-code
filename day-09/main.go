fa143fa79009deb266fed55f8b09c99fbce9076a
danprince <danielprince@hotmail.co.uk>
2018-12-09 09:35:14 +0000

    adds solutions for day 9

---
+++ day-09/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Play(players, turns int) int {
+	scores := map[int]int{}
+	circle := ring.New(1)
+	circle.Value = 0
+
+	for turn := 0; turn <= turns; turn++ {
+		player := turn % players
+
+		if turn%23 == 0 {
+			circle = circle.Move(-8)
+			marble := circle.Unlink(1)
+			circle = circle.Move(1)
+			scores[player] += turn + marble.Value.(int)
+		} else {
+			marble := ring.New(1)
+			marble.Value = turn
+			circle = circle.Move(1).Link(marble).Move(-1)
+		}
+	}
+
+	highscore := 0
+
+	for _, score := range scores {
+		if score > highscore {
+			highscore = score
+		}
+	}
+
+	return highscore
+}
+
+func SolvePartOne(input string) int {
+	parts := strings.Split(input, " ")
+	players, _ := strconv.Atoi(parts[0])
+	limit, _ := strconv.Atoi(parts[6])
+	return Play(players, limit)
+}
+
+func SolvePartTwo(input string) int {
+	parts := strings.Split(input, " ")
+	players, _ := strconv.Atoi(parts[0])
+	limit, _ := strconv.Atoi(parts[6])
+	return Play(players, limit*100)
+}
+
+func main() {
+	bytes, _ := ioutil.ReadAll(os.Stdin)
+	input := string(bytes)
+	fmt.Println("Part 1:", SolvePartOne(input))
+	fmt.Println("Part 2:", SolvePartTwo(input))
+}
---
+++ day-09/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func TestSolvePartOne(t *testing.T) {
+	examples := map[string]int{
+		"9 players; last marble is worth 25 points":    32,
+		"10 players; last marble is worth 1618 points": 8317,
+		"13 players; last marble is worth 7999 points": 146373,
+		"17 players; last marble is worth 1104 points": 2764,
+		"21 players; last marble is worth 6111 points": 54718,
+		"30 players; last marble is worth 5807 points": 37305,
+	}
+
+	for input, expected := range examples {
+		actual := SolvePartOne(input)
+
+		if actual != expected {
+			t.Errorf(`Part 1: Expected: %#v Actual: %#v`, expected, actual)
+		}
+	}
+}
+
+func TestSolvePartTwo(t *testing.T) {
+	examples := map[string]int{}
+
+	for input, expected := range examples {
+		actual := SolvePartTwo(input)
+
+		if actual != expected {
+			t.Errorf(`Part 2: Expected: %#v Actual: %#v`, expected, actual)
+		}
+	}
+}
