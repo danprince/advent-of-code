a605dd0eb97639b7463c519dde211e20cc530c5c
danprince <northwalesdan@gmail.com>
2019-12-02 11:43:17 +0000

    adds day 2 solutions

---
+++ 2019/day-02/src/main.rs
@@ -0,0 +1,115 @@
+use std::fs;
+
+type Program = Vec<usize>;
+
+mod opcodes {
+    pub const ADD: usize = 1;
+    pub const MUL: usize = 2;
+    pub const HALT: usize = 99;
+}
+
+fn main() {
+    println!("Part 1: {}", part_one());
+    println!("Part 2: {}", part_two());
+}
+
+fn part_one() -> usize {
+    let src = fs::read_to_string("./input/input.txt")
+        .unwrap()
+        .trim()
+        .to_string();
+
+    let mut program = parse_program(&src);
+
+    program[1] = 12;
+    program[2] = 2;
+
+    program = run_program(program);
+    program[0]
+}
+
+fn part_two() -> usize {
+    let src = fs::read_to_string("./input/input.txt")
+        .unwrap()
+        .trim()
+        .to_string();
+
+    let program = parse_program(&src);
+
+    for noun in 0..100 {
+        for verb in 0..100 {
+            let mut local_program = program.clone();
+            local_program[1] = noun;
+            local_program[2] = verb;
+            local_program = run_program(local_program);
+
+            if local_program[0] == 19690720 {
+                return 100 * noun + verb
+            }
+        }
+    }
+
+    panic!("Couldn't find inputs")
+}
+
+fn parse_program(src: &str) -> Program {
+    src.split(",")
+        .map(|c| c.parse::<usize>().unwrap())
+        .collect()
+}
+
+fn run_program(mut program: Program) -> Program {
+    let mut ip = 0;
+
+    while ip < program.len() {
+        let op = program[ip];
+
+        match op {
+            opcodes::ADD => {
+                let a = program[ip + 1];
+                let b = program[ip + 2];
+                let c = program[ip + 3];
+                program[c] = program[a] + program[b];
+                ip += 4;
+            }
+            opcodes::MUL => {
+                let a = program[ip + 1];
+                let b = program[ip + 2];
+                let c = program[ip + 3];
+                program[c] = program[a] * program[b];
+                ip += 4;
+            }
+            opcodes::HALT => break,
+            code => panic!("Unknown op code {}", code),
+        }
+    }
+
+    program
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_examples() {
+        assert_eq!(
+            run_program(vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]),
+            vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+        );
+
+        assert_eq!(run_program(vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
+
+        assert_eq!(run_program(vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
+
+        assert_eq!(
+            run_program(vec![2, 4, 4, 5, 99, 0]),
+            vec![2, 4, 4, 5, 99, 9801]
+        );
+
+        assert_eq!(
+            run_program(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
+            vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
+        );
+    }
+}
