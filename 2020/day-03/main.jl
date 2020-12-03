e19efa4af89cb12a231fe7e24c4260dba23ae9e4
danprince <northwalesdan@gmail.com>
2020-12-03 10:41:32 +0000

    adds day 3 solution

---
+++ 2020/day-03/main.jl
@@ -0,0 +1,46 @@
+using Test
+
+function part1(input)
+    grid = split(input, "\n")
+    count_trees(grid, [3, 1])
+end
+
+function part2(input)
+    grid = split(input, "\n")
+    moves = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    prod([count_trees(grid, move) for move in moves])
+end
+
+function count_trees(grid, move)
+    x = 1
+    dx, dy = move
+    rows = length(grid)
+    cols = length(grid[1])
+
+    count(1:dy:rows) do y
+        c = grid[y][x] == '#'
+        x = (x + dx - 1) % cols + 1
+        return c
+    end
+end
+
+example = "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"
+
+@test part1(example) == 7
+@test part2(example) == 336
+
+open("input.txt") do file
+    input = read(file, String) |> strip
+    println("Part 1: $(part1(input))")
+    println("Part 2: $(part2(input))")
+end
